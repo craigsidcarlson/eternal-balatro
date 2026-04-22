6226b68ec700ec063fe14f5916c3b8f22338c2a1
Craigums <craigsidcarlson@gmail.com>
2026-04-22 14:07:34 -0600

    Adding a mod to make a dedicated white stake with eternals

---
+++ Enterals/Eternals.lua
@@ -0,0 +1,24 @@
+SMODS.Stake({
+    name = "Eternals",
+    key = "eternals",
+    colour = HEX("FC46AA"),
+    above_stake = "stake_white",
+    applied_stakes = { "stake_white" },
+    pos = { x = 3, y = 3 },
+    prefix_config = {
+        above_stake = { mod = false },
+        applied_stakes = { mod = false }
+    },
+    modifiers = function()
+        G.GAME.modifiers.enable_eternals_in_shop = true
+    end,
+    unlocked = true,
+
+    loc_txt = {
+        name = "Eternals",
+        text = {
+            "Jokers may be {C:eternal}Eternal{}",
+            "{s:0.8}(Cannot be sold or destroyed)"
+        }
+    }
+})
